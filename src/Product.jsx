b02f7db1f9f624e9a7000d18cb2257d983651c97
Joseph Oluwapelumi <joluwapelumi027@gmail.com>
2025-08-28 05:14:43 +0100

    fixed cart design issue

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,18 @@
 import './Product.css'
-export default function Product({ product }) {
+export default function Product({ product , addToCart}) {
     return (
         <div className="product">
+                   
             <div className='product-image-container'>
                 <picture>
                     <source media="(min-width: 600px)" srcSet={product.image.tablet} />
                     <source media="(min-width: 900px)" srcSet={product.image.desktop} />
-                    <img src={product.image.mobile} alt="Product Image" />
+                    <img src={product.image.mobile} alt="Product Image" className= "product-image"/>
                 </picture>
-                <button className='image-btn'>Add to Cart</button>
+                <button onClick={() => addToCart(product)} className='add-to-cart-button'>
+                    <img src="/assets/images/icon-add-to-cart.svg" alt="Add to Cart" className='add-to-cart-icon'/>
+                    <span>Add to Cart</span>
+                </button>
             </div>
             <div>
                 <p className='product-description'>{product.category}</p>
--- src/Productlist.jsx
+++ src/Productlist.jsx
@@ -3,7 +3,15 @@ import './Productlist.css'
 import { useEffect, useState } from "react"
 export default function ProductList() {
     const [products, setProducts] = useState([])
-    
+    const [cart, setCart] = useState([])
+    const addToCart = (product)=>{
+        setCart(prevCart=>{
+            const existing = prevCart.find(item=>{
+                item.id=== product.id ? {...item, quantity: item.quantity+1} : item
+            })
+           return [...prevCart,{...product, quantity:1}]
+        })
+    }
     useEffect(() => {
         fetch('/data.json')
             .then(response => response.json())
@@ -15,7 +23,7 @@ export default function ProductList() {
             <h1>Desserts</h1>
             <div className="products">
                 {products.map(product => (
-                    <Product key={product.id} product={product} />
+                    <Product key={product.id} product={product} addToCart= {addToCart}/>
                 ))}
             </div>
             </div>
